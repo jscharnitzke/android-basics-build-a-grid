4183c3d0ed897141849ee1d01fb22217f20b39b3
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-14 16:20:38 -0500

    add grid to display all topics

--- app/src/main/java/com/example/topics/MainActivity.kt
+++ app/src/main/java/com/example/topics/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,6 +36,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +55,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    TopicGrid(
+                        modifier = Modifier.fillMaxSize(),
+                        topics = DataSource.topics
+                    )
                 }
             }
         }
@@ -52,19 +72,43 @@ data class Topic(
 )
 
 @Composable
+fun TopicGrid(
+    modifier: Modifier = Modifier,
+    topics: List<Topic>,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(topics) { topic ->
+            TopicCard(
+                topic = topic,
+            )
+        }
+    }
+}
+
+@Composable
 fun TopicCard(
     modifier: Modifier = Modifier,
     topic: Topic,
 ) {
-    Card {
-        Row {
+    Card(
+        modifier = modifier
+            .height(68.dp)
+    ) {
+        Row(modifier = modifier.weight(1f)) {
             Image(
                 contentDescription = stringResource(id = topic.nameResourceId),
-                modifier = modifier.height(68.dp).width(68.dp),
+                modifier = modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f, true),
                 painter = painterResource(id = topic.imageResourceId),
             )
 
-            TopicDetails(modifier = modifier, topic = topic)
+            TopicDetails(topic = topic)
         }
     }
 }
@@ -76,7 +120,7 @@ fun TopicDetails(
 ) {
     Column {
         Text(
-            modifier = modifier.padding(
+            modifier = Modifier.padding(
                 bottom = 8.dp,
                 end = 16.dp,
                 start = 16.dp,
@@ -87,12 +131,12 @@ fun TopicDetails(
         )
 
         Row(
+            horizontalArrangement = Arrangement.Start,
             modifier = modifier.padding(start = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Icon(
                 contentDescription = null,
-                modifier = modifier,
                 painter = painterResource(id = R.drawable.ic_grain),
             )
 
