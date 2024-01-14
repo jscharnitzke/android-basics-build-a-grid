058f299eec83b4f17338bd13d8a37a399ec055cf
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-14 14:56:06 -0500

    basic styling to meet requirements

--- app/src/main/java/com/example/topics/MainActivity.kt
+++ app/src/main/java/com/example/topics/MainActivity.kt
@@ -8,17 +8,24 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.topics.model.DataSource
 import com.example.topics.ui.theme.TopicsTheme
 
@@ -53,6 +60,7 @@ fun TopicCard(
         Row {
             Image(
                 contentDescription = stringResource(id = topic.nameResourceId),
+                modifier = modifier.height(68.dp).width(68.dp),
                 painter = painterResource(id = topic.imageResourceId),
             )
 
@@ -67,16 +75,32 @@ fun TopicDetails(
     topic: Topic,
 ) {
     Column {
-        Text(modifier = modifier, text = stringResource(id = topic.nameResourceId))
+        Text(
+            modifier = modifier.padding(
+                bottom = 8.dp,
+                end = 16.dp,
+                start = 16.dp,
+                top = 16.dp,
+            ),
+            style = MaterialTheme.typography.bodyMedium,
+            text = stringResource(id = topic.nameResourceId),
+        )
 
-        Row {
-            Image(
+        Row(
+            modifier = modifier.padding(start = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
                 contentDescription = null,
                 modifier = modifier,
                 painter = painterResource(id = R.drawable.ic_grain),
             )
 
-            Text(modifier = modifier, text = "${topic.courseCount}")
+            Text(
+                modifier = modifier.padding(start = 8.dp),
+                style = MaterialTheme.typography.labelMedium,
+                text = "${topic.courseCount}",
+            )
         }
     }
 }
