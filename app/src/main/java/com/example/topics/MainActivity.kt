8714a776543d7ab46e0b9d66bb69fd1aeb45e0cb
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-14 14:43:17 -0500

    include base data in topic card

--- app/src/main/java/com/example/topics/MainActivity.kt
+++ app/src/main/java/com/example/topics/MainActivity.kt
@@ -5,14 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.topics.model.DataSource
 import com.example.topics.ui.theme.TopicsTheme
@@ -46,7 +51,32 @@ fun TopicCard(
 ) {
     Card {
         Row {
+            Image(
+                contentDescription = stringResource(id = topic.nameResourceId),
+                painter = painterResource(id = topic.imageResourceId),
+            )
 
+            TopicDetails(modifier = modifier, topic = topic)
+        }
+    }
+}
+
+@Composable
+fun TopicDetails(
+    modifier: Modifier = Modifier,
+    topic: Topic,
+) {
+    Column {
+        Text(modifier = modifier, text = stringResource(id = topic.nameResourceId))
+
+        Row {
+            Image(
+                contentDescription = null,
+                modifier = modifier,
+                painter = painterResource(id = R.drawable.ic_grain),
+            )
+
+            Text(modifier = modifier, text = "${topic.courseCount}")
         }
     }
 }
